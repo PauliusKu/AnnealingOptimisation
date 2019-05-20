cff6a46caffad4c5be7ab5ee2c39821804a3bc36
PauliusKu <paulius.kuprevicius@gmail.com>
2019-05-20 22:20:48 +0300

    Added README.md

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+double func(double x, double y){
+    return -((x * y - x * x * y - x * y * y) / 8);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double x = 0;
+    double y = 0;
+
+    std::cout << func(x, y) << std::endl;
+
     return 0;
 }
