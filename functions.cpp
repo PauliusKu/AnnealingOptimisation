c83177c7fcad5dcfdd08e318c7116f15c97063ee
PauliusKu <paulius.kuprevicius@gmail.com>
2019-05-21 19:11:08 +0300

    Added functions.h

---
+++ functions.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Paulius on 2019-05-21.
+//
+
+#include <cmath>
+#include "functions.h"
+
+double funcBooth(const double x1, const double x2){
+    return pow(x1 + 2 * x2 - 7, 2) + pow(2 * x1 + x2 - 5, 2);
+}
---
+++ functions.h
@@ -0,0 +1,10 @@
+//
+// Created by Paulius on 2019-05-21.
+//
+
+#ifndef ANNEALINGOPTIMISATION_FUNCTIONS_H
+#define ANNEALINGOPTIMISATION_FUNCTIONS_H
+
+#endif //ANNEALINGOPTIMISATION_FUNCTIONS_H
+
+double funcBooth(double, double);
