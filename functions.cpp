b9fc05ecc6fe55998f79e713528b38aa91b6ff42
Vladyslav Palkevič <vladyslav.palkevic@mif.stud.vu.lt>
2019-05-30 00:42:51 +0300

    Pataisytos funk

--- functions.cpp
+++ functions.cpp
@@ -32,18 +32,19 @@ double funcMatyas(std::vector<double> &X){
 }
 
 double funcGoldsteinAndPrice(std::vector<double>&X){
-    return (1+pow(X[0]+X[1]+1,2)*(19-14*X[0]+13*pow(X[1],2)-14*X[1]+6*X[0]*X[1]+3*pow(X[1],2)
-    *(30+pow(2*X[0]-3*X[1],2)*(18-32*X[0]+12*pow(X[1],2)-48*X[1]-36*X[0]*X[1]+27*pow(X[1],2)))));
+   double a,b;
+    a = 1+pow(X[0]+X[1]+1,2)*(19-14*X[0]+3*pow(X[0],2)-14*X[1]+6*X[0]*X[1]+3*pow(X[1],2));
+    b = 30+pow(2*X[0]-3*X[1],2)*(18-32*X[0]+12*pow(X[0],2)+48*X[1]-36*X[0]*X[1]+27*pow(X[1],2));
+    return a*b;
+
 }
 
 double funcEasom(std::vector<double>&X){
-    return -1*std::cos(X[1])*std::cos(X[1])*pow(M_E,-1*pow(X[0]-M_PI, 2)- pow(X[1]-M_PI, 2));
+    return -1*std::cos(X[0])*std::cos(X[1])*M_E*(-pow(X[0]- M_PI,2)-pow(X[1]-M_PI,2));
 }
 
 double funcBeale(std::vector<double>&X){
-    return pow(1.5-X[0]+X[0]*X[1],2)+
-            pow(2.25-X[0]+X[0]*X[1],2)+
-            pow(2.625-X[0]+X[0]*X[1],2);
+    return pow(1.5-X[0]*(1-X[1]),2)+pow(2.25-X[0]*(1-pow(X[1],2)),2)+pow(2.625-X[0]*(1-pow(X[1],3)),2);
 }
 double funcBranin(std::vector<double>&X){
     return pow(X[1]-(5.1/(4*pow(M_PI,2)))*pow(X[0],2)+5*X[0]/M_PI-6,2)+10*(1-1/(8*M_PI))*std::cos(X[0])+10;
